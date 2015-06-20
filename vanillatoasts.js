4bb4ca0ce4c476f5cd3c690e2fbb50f68d7cd256
AlexKvazos <contact@alexkvazos.com>
2015-06-20 18:27:42 -0500

    Fixed CommonJS export

--- vanillatoasts.js
+++ vanillatoasts.js
@@ -1,102 +1,93 @@
 (function(root, factory) {
   // commonjs
   if (typeof exports === 'object') {
-    factory(function(VanillaToasts) {
-      module.exports = VanillaToasts;
-    });
+    module.exports = factory();
   // global
   } else {
-    factory(function(VanillaToasts) {
-      root.VanillaToasts = VanillaToasts;
-    });
+    root.VanillaToasts = factory();
   }
-})(this, function(ready) {
+})(this, function() {
 
   // We need DOM to be ready
   if (document.readyState === 'complete') {
-    init(ready);
+    init();
   } else {
-    window.addEventListener('DOMContentLoaded', function() {
-      init(ready);
-    });
+    window.addEventListener('DOMContentLoaded', init);
   }
 
   // Initialize library
-  function init(ready) {
-
+  function init() {
     // Toast container
     var container = document.createElement('div');
     container.id = 'vanillatoasts-container';
     document.body.appendChild(container);
+  }
 
+  var VanillaToasts = {};
+  var autoincrement = 0;
 
-    var VanillaToasts = {};
-    var autoincrement = 0;
+  VanillaToasts.create = function(options) {
+    var toast = document.createElement('div');
+    toast.id = ++autoincrement;
+    toast.id = 'toast-' + toast.id;
+    toast.className = 'vanillatoasts-toast';
 
-    VanillaToasts.create = function(options) {
-      var toast = document.createElement('div');
-      toast.id = ++autoincrement;
-      toast.id = 'toast-' + toast.id;
-      toast.className = 'vanillatoasts-toast';
+    // title
+    if (options.title) {
+      var h4 = document.createElement('h4');
+      h4.className = 'vanillatoasts-title';
+      h4.innerHTML = options.title;
+      toast.appendChild(h4);
+    }
 
-      // title
-      if (options.title) {
-        var h4 = document.createElement('h4');
-        h4.className = 'vanillatoasts-title';
-        h4.innerHTML = options.title;
-        toast.appendChild(h4);
-      }
+    // text
+    if (options.text) {
+      var p = document.createElement('p');
+      p.className = 'vanillatoasts-text';
+      p.innerHTML = options.text;
+      toast.appendChild(p);
+    }
 
-      // text
-      if (options.text) {
-        var p = document.createElement('p');
-        p.className = 'vanillatoasts-text';
-        p.innerHTML = options.text;
-        toast.appendChild(p);
-      }
+    // icon
+    if (options.icon) {
+      var img = document.createElement('img');
+      img.src = options.icon;
+      img.className = 'vanillatoasts-icon';
+      toast.appendChild(img);
+    }
 
-      // icon
-      if (options.icon) {
-        var img = document.createElement('img');
-        img.src = options.icon;
-        img.className = 'vanillatoasts-icon';
-        toast.appendChild(img);
-      }
+    // click callback
+    if (typeof options.callback === 'function') {
+      toast.addEventListener('click', options.callback);
+    }
 
-      // click callback
-      if (typeof options.callback === 'function') {
-        toast.addEventListener('click', options.callback);
-      }
-
-      // toast api
-      toast.hide = function() {
-        toast.className += ' vanillatoasts-fadeOut';
-        toast.addEventListener('animationend', removeToast, false);
-      };
-
-      // autohide
-      if (options.timeout) {
-        setTimeout(toast.hide, options.timeout);
-      }
-
-      if (options.type) {
-        toast.className += ' vanillatoasts-' + options.type;
-      }
-
-      toast.addEventListener('click', toast.hide);
-
-
-      function removeToast() {
-        document.getElementById('vanillatoasts-container').removeChild(toast);
-      }
-
-      document.getElementById('vanillatoasts-container').appendChild(toast);
-      return toast;
-
+    // toast api
+    toast.hide = function() {
+      toast.className += ' vanillatoasts-fadeOut';
+      toast.addEventListener('animationend', removeToast, false);
     };
 
-    ready(VanillaToasts);
+    // autohide
+    if (options.timeout) {
+      setTimeout(toast.hide, options.timeout);
+    }
 
-  }
+    if (options.type) {
+      toast.className += ' vanillatoasts-' + options.type;
+    }
+
+    toast.addEventListener('click', toast.hide);
+
+
+    function removeToast() {
+      document.getElementById('vanillatoasts-container').removeChild(toast);
+    }
+
+    document.getElementById('vanillatoasts-container').appendChild(toast);
+    return toast;
+
+  };
+
+  return VanillaToasts;
 
 });
